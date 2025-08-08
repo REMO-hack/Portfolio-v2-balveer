6d4441d8c761530e78e1f538b4c616cf6fb08041
REMO-hack <patel.balveer07@gmail.com>
2025-08-08 11:23:38 +0530

    Update Project.tsx

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -8,7 +8,7 @@ import ScrollAnimation from "react-animate-on-scroll";
 export function Project() {
   return (
     <Container id="project">
-      <h2>Services</h2>
+      <h2>Projects</h2>
       <div className="projects">
 
         <ScrollAnimation animateIn="flipInX">
@@ -22,9 +22,9 @@ export function Project() {
 
             </header>
             <div className="body">
-              <h3>Digital Marketing & Strategy</h3>
+              <h3>Campus Food Delivery System</h3>
               <p>
-                I help individuals and small businesses enhance their online presence through tailored digital marketing strategies, social media optimization, and content planning—ensuring steady growth and client engagement.
+                Streamlined food ordering and logistics across college campuses
               </p>
             </div>
           </div>
@@ -40,8 +40,8 @@ export function Project() {
 
             </header>
             <div className="body">
-              <h3>Resume Building - Stand Out with a Winning Resume</h3>
-              <p> I design clean, professional resumes that highlight your strengths and achievements—customized to suit your industry and grab the attention of recruiters and hiring managers.
+              <h3>Study Group Connector</h3>
+              <p> Matched students based on interests and schedules for peer-based learning.
               </p>
             </div>
 
@@ -51,7 +51,7 @@ export function Project() {
 
 
 
-        {/* <ScrollAnimation animateIn="flipInX">
+         <ScrollAnimation animateIn="flipInX">
           <div className="project">
             <header>
               <svg width="50" xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" fill="none" stroke="#23ce6b " stroke-width="1" stroke-linecap="round" stroke-linejoin="round" ><title>Folder</title><path d="M22 19a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h5l2 3h9a2 2 0 0 1 2 2z"></path></svg>
@@ -62,21 +62,14 @@ export function Project() {
               </div>
             </header>
             <div className="body">
-              <h3>VeloCityAI</h3>
+              <h3>Digital Marketing Toolkit (Chrome Extension)</h3>
               <p>
-                VelocityAI is a website that allows users to get instant answers to any question and generate stunning AI pictures using OpenAI's GPT-3 and DALL-E model.With VelocityAI, users can type in text-based prompts and receive a corresponding response.
+             A productivity tool for marketers to manage campaigns and insights
               </p>
             </div>
-            <footer>
-              <ul className="tech-list">
-                <li>React</li>
-                <li>Typescript</li>
-                <li>Html</li>
-                <li>css</li>
-              </ul>
-            </footer>
+           
           </div>
-        </ScrollAnimation> */}
+        </ScrollAnimation> 
 
         {/* <ScrollAnimation animateIn="flipInX">
           <div className="project">
